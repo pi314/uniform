8fd543f283e88a648e7cc43ce6b0fc5e399f17e2
Apua <Apua.A.Aa@gmail.com>
2015-02-28 21:22:39 +0800

    Refactor

--- __init__.py
+++ __init__.py
@@ -4,17 +4,17 @@ r'''
 DocTest
 =======
 
->>> print_ = lambda *a, **k: print(file=__import__('sys').stdout, *a, **k)
+>>> p = lambda data, **kwargs : builtins.print(sep='\n', *do(data, **kwargs))
 >>> one_dim_data = ['a','b','c','aaa','bbb','ccc']
->>> print_(one_dim_data, cols=3)
+>>> p(one_dim_data, cols=3)
 a   b   c
 aaa bbb ccc
->>> print_(one_dim_data, width=8)
+>>> p(one_dim_data, width=8)
 a   b
 c   aaa
 bbb ccc
 >>> csv_data = ['a,b,c',',bbb,ccc']
->>> print_(csv_data, delimiter=',')
+>>> p(csv_data, delimiter=',')
 a b   c
   bbb ccc
 >>> two_dim_data = [
@@ -22,7 +22,7 @@ a b   c
 ...     ['aaaa', 'bbb', 'cc'],
 ...     ['aa', 'bbbbbb', 'cccc']
 ... ]
->>> print_(two_dim_data)
+>>> p(two_dim_data)
 a    bb     ccc
 aaaa bbb    cc
 aa   bbbbbb cccc
@@ -32,11 +32,6 @@ aa   bbbbbb cccc
 import itertools
 import sys
 import builtins
-
-
-def strlen (s):
-    ''' Calculate the **visual** width of string s '''
-    return len(s)
 
 
 def _to_2d (data, cols):
@@ -50,36 +45,28 @@ def _do_2d (data, border=' '):
     return [ border.join( j[0].ljust(j[1]) for j in zip(i, widths) ).rstrip() for i in data]
 
 
-def do (data, width=None, cols=None, delimiter=None, border=' '):
-    ''' Make input data column-aligned '''
-    if width is not None:
-        # data should be in [str, str, str] format
-        ...
-
-    if cols is not None:
-        # data should be in [str, str, str] format
-        return _do_2d( _to_2d(data, cols), border=border )
-
-    if delimiter is not None:
-        # data should be in [str, str, str] format
-        return _do_2d( [i.split(delimiter) for i in data], border=border )
-
-    else:
-        # data should be in [str, str, str] format
-        return _do_2d( [i.split() for i in data], border=border )
-
-    if all( isinstance(i, str) for i in data ):
-        # data should be in [str, str, str] format
-        return None
-
-    return _do_2d(data, border=border)
-
-
 def _colwidth_2d (data):
     ''' Calculate the width of every column of a 2D list'''
+    builtins.print(data)
     l = max( len(i) for i in data )
     z = list( itertools.zip_longest(*data, fillvalue='') )
-    return [max(strlen(j) for j in z[i]) for i in range(l)]
+    return [max(len(j) for j in z[i]) for i in range(l)]
+
+
+def do (data, width=None, cols=None, delimiter=None, border=' '):
+    ''' Make input data column-aligned '''
+
+    if any(not isinstance(i, str) for i in data ):
+        return _do_2d(data, border=border)
+
+    if width is not None:
+        return _do_2d( [i.split() for i in data], border=border )
+    elif cols is not None:
+        return _do_2d( _to_2d(data, cols), border=border )
+    elif delimiter is not None:
+        return _do_2d( [i.split(delimiter) for i in data], border=border )
+    else:
+        ...
 
 
 def colwidth (data, width=None, cols=None, delimiter=None):
@@ -123,9 +110,9 @@ def run_command ():
     group.add_argument('-d', '--delimiter',type=str, dest='delimiter')
 
     kwargs = vars(parser.parse_args())
-    data = [line.rstrip() for line in sys.stdin]
-    print(data, **kwargs)
 
+    for row in do([line.rstrip() for line in sys.stdin], **kwargs):
+        builtins.print(row)
 
 
 if __name__ == '__main__':

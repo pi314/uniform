f51eaebfb2d83acc86bcc7867ad16814854ab776
Apua <Apua.A.Aa@gmail.com>
2015-02-28 17:50:08 +0800

    Fixed coding style
    Adding empty lines between functions,
    and make arguments to multi-lines since too many.

--- __init__.py
+++ __init__.py
@@ -4,18 +4,22 @@ import sys
 import builtins
 import argparse
 
+
 def strlen (s):
     ''' Calculate the **visual** width of string s '''
     return len(s)
+
 
 def _to_2d (data, cols):
     ''' Change 1D list data into a 2D list according to cols '''
     return [ data[i:i+cols] for i in range(0, len(data), cols) ]
 
+
 def _do_2d (data, border=' '):
     ''' Make 2D input data column-aligned '''
     widths = _colwidth_2d(data)
     return [ border.join( j[0].ljust(j[1]) for j in zip(i, widths) ).rstrip() for i in data]
+
 
 def do (data, width=None, cols=None, delimiter=None, border=' '):
     ''' Make input data column-aligned '''
@@ -41,11 +45,13 @@ def do (data, width=None, cols=None, delimiter=None, border=' '):
 
     return _do_2d(data, border=border)
 
+
 def _colwidth_2d (data):
     ''' Calculate the width of every column of a 2D list'''
     l = max( len(i) for i in data )
     z = list( itertools.zip_longest(*data, fillvalue='') )
     return [max(strlen(j) for j in z[i]) for i in range(l)]
+
 
 def colwidth (data, width=None, cols=None, delimiter=None):
     ''' Calculate the with of every column of input data '''
@@ -70,9 +76,12 @@ def colwidth (data, width=None, cols=None, delimiter=None):
 
     return _colwidth_2d(data)
 
-def print (data, width=None, cols=None, delimiter=None, border=' ', file=sys.stdout, flush=False):
+
+def print (data, width=None, cols=None, delimiter=None, border=' ',
+           file=sys.stdout, flush=False):
     for i in do(data, width=width, cols=cols, delimiter=delimiter, border=border):
         builtins.print(i, file=file, flush=flush)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A tool that makes text column-aligned.')
@@ -85,5 +94,6 @@ if __name__ == '__main__':
     data = [line.rstrip() for line in sys.stdin()]
 
     builtins.print(args)
-    print(data, width=args.width, cols=args.columns, delimiter=args.delimiter, border=args.border)
+    print(data, width=args.width, cols=args.columns,
+          delimiter=args.delimiter, border=args.border)
 

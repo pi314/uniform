ccac990418f7f99e39200c862065e7750db6cb77
Apua <Apua.A.Aa@gmail.com>
2015-03-01 12:56:55 +0800

    Removed unused functions, refactored assertion, ...
    `width` feature has not been finished implement yet

--- __init__.py
+++ __init__.py
@@ -5,20 +5,20 @@ DocTest
 =======
 
 >>> p = lambda data, **kwargs : print(sep='\n', *do(data, **kwargs))
+>>> one_dim_data = ['a','b','c','aaa','bbb','ccc']
+>>> p(one_dim_data, cols=3)
+a   b   c
+aaa bbb ccc
 
-.. >>> one_dim_data = ['a','b','c','aaa','bbb','ccc']
-.. >>> p(one_dim_data, cols=3)
-.. a   b   c
-.. aaa bbb ccc
 .. >>> p(one_dim_data, width=8)
 .. a   b
 .. c   aaa
 .. bbb ccc
-.. >>> csv_data = ['a,b,c',',bbb,ccc']
-.. >>> p(csv_data, delimiter=',')
-.. a b   c
-..   bbb ccc
 
+>>> csv_data = ['a,b,c',',bbb,ccc']
+>>> p(csv_data, delimiter=',')
+a b   c
+  bbb ccc
 >>> two_dim_data = [
 ...     ['a', 'bb', 'ccc'],
 ...     ['aaaa', 'bbb', 'cc'],
@@ -31,43 +31,58 @@ aa   bbbbbb cccc
 '''
 
 
-def _to_2d (data, cols):
-    ''' Change 1D list data into a 2D list according to cols '''
-    return [ data[i:i+cols] for i in range(0, len(data), cols) ]
-
-
-def _do_2d (data, border=' '):
-    ''' Make 2D input data column-aligned '''
-    widths = _colwidth_2d(data)
-    return [ border.join( j[0].ljust(j[1]) for j in zip(i, widths) ).rstrip() for i in data]
-
-
-def _colwidth_2d (data):
-    import itertools
-
-    l = max( len(i) for i in data )
-    z = list( itertools.zip_longest(*data, fillvalue='') )
-    L = [max(len(j) for j in z[i]) for i in range(l)]
-    return L
-
-
 def get_colwidth(cols):
+    '''
+    >>> cols = (('a', 'aa'), ('bbbb', 'b'))
+    >>> get_colwidth(cols)
+    (2, 4)
+    '''
     return tuple(max(map(len, col)) for col in cols)
 
 
 def trans_2dim_cols(rows):
+    '''
+    >>> rows = ((1, 2), (3,))
+    >>> trans_2dim_cols(rows)
+    ((1, 3), (2, ''))
+    '''
     from itertools import zip_longest
     return tuple(zip_longest(fillvalue='', *rows))
 
 
-def arrange_seq(seq, length):
+def arrange_seq_cols(seq, length):
+    '''
+    >>> seq = (0,1,2,3,4,5)
+    >>> arrange_seq_cols(seq, 3)
+    ((0, 3), (1, 4), (2, 5))
+    >>> arrange_seq_cols(seq, 2)
+    ((0, 2, 4), (1, 3, 5))
+    '''
     return tuple(seq[start::length] for start in range(length))
 
 
-def clean(data, *, key=None, val=None):
-    r'''
-    return cleaned columns
+def find_colwidth(seq, offset, max_length):
+    '''
+    >>> find_colwidth((1,2,2,1,1), 1, 6)
+    (2, 2)
+    >>> find_colwidth((1,2,1,1,2), 1, 6)
+    (1, 2, 1)
+    '''
+    # shorten test values
+    num_cols = 1
+    sorted_seq = sorted(seq, reverse=True)
+    while 1:
+        if sum(sorted_seq[:num_cols+1]) + offset*num_cols <= max_length:
+            num_cols += 1
+        else:
+            break
 
+    # try more columns
+
+
+
+def clean(data, *, key=None, val=None, border=' '):
+    r'''
     >>> clean([['a','b'],['c','d']])
     ((('a', 'c'), ('b', 'd')), (1, 1))
     >>> clean(['a,b', 'c,d'], key='delimiter', val=',')
@@ -84,18 +99,20 @@ def clean(data, *, key=None, val=None):
         cols = trans_2dim_cols(row.split(val) for row in data)
         return cols, get_colwidth(cols)
     elif key=='cols':
-        offset = val - len(data) % 3
-        cols = arrange_seq(seq=tuple(data)+('',)*offset, length=val)
+        remainder = len(data) % 3
+        offset = val - remainder if remainder else 0
+        cols = arrange_seq_cols(seq=tuple(data)+('',)*offset, length=val)
         return cols, get_colwidth(cols)
     elif key=='width':
-        ...
+        widths = tuple(map(len, data))
+        print('widths:', widths)
+        colwidth = find_colwidth(widths, offset=len(border), max_length=val)
+        print('colwidth', colwidth)
 
 
 
 def gen_combined_rows(cols, colwidth, border):
     r'''
-    given columns, generate combined rows
-
     >>> cols = [['a','aa'], ['b','bbbb'], ['c','ccc']]
     >>> colwidth = (2, 4, 3)
     >>> c_rows = gen_combined_rows(cols, colwidth, '|')
@@ -124,43 +141,22 @@ def do (data, width:int=None, cols:int=None, delimiter:str=None, border=' '):
     elif all(all(isinstance(item, str) for item in row) for row in data):
         is_2dim = True
     else:
-        raise Exception
+        raise AssertionError
 
     # check parameters and generate process
-    annotations = do.__annotations__
+    vars_ = vars()
+    annos = do.__annotations__
+    ex_args = tuple((k,t,vars_[k]) for k,t in annos.items())
     if is_2dim:
-        for k in annotations:
-            if locals()[k] is not None:
-                raise Exception
+        assert sum(v is None for k,t,v in ex_args)==3
         cols, colwidth = clean(data)
     else:
-        paras = None
-        for k, t in annotations.items():
-            val = locals()[k]
-            if val is None:
-                continue
-            if not isinstance(val, t):
-                raise Exception
-            if paras is not None:
-                raise Exception
-            paras = {'key':k, 'val':val}
-        cols, colwidth = clean(data, **paras)
+        assert sum(v is None for k,t,v in ex_args)==2
+        key, type_, val = next(a for a in ex_args if a[2] is not None)
+        assert isinstance(val, type_), (key, type_, val)
+        cols, colwidth = clean(data, border=border, key=key, val=val)
 
     return gen_combined_rows(cols, colwidth=colwidth, border=border)
-
-    '''
-    if any(not isinstance(i, str) for i in data ):
-        return _do_2d(data, border=border)
-
-    if width is not None:
-        return _do_2d( [i.split() for i in data], border=border )
-    elif cols is not None:
-        return _do_2d( _to_2d(data, cols), border=border )
-    elif delimiter is not None:
-        return _do_2d( [i.split(delimiter) for i in data], border=border )
-    else:
-        ...
-    '''
 
 
 def run_command ():
